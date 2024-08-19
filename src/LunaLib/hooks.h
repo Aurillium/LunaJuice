374e7a8b2ec2a20209312374c878ba67db4590a1
Memphis Marshall (~aurillium) <memphism2003@gmail.com>
2024-08-19 08:14:38 +0800

    V3 the default for this version

--- src/LunaLib/hooks.h
+++ src/LunaLib/hooks.h
@@ -11,7 +11,7 @@
 #define QUICK_HOOK_V3(dll, name) (InstallHookV3(dll, #name, (void*)Hooked_##name, (void**)&Real_##name))
 #define QUICK_HOOK_V2(dll, name) (InstallHookV2(dll, #name, (void*)Hooked_##name, (void**)&Real_##name))
 // This is more stable than V3, so keep it the default
-#define QUICK_HOOK(dll, name) (InstallHookV2(dll, #name, (void*)Hooked_##name, (void**)&Real_##name))
+#define QUICK_HOOK(dll, name) (InstallHookV3(dll, #name, (void*)Hooked_##name, (void**)&Real_##name))
 #define EXTERN_HOOK(name) extern name##_t Real_##name;
 
 // Quickly define hooks
